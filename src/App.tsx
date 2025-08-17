0ef1052b727c19e7015760b56b7f423a12f4bde9
PeterFreemann <128150016+PeterFreemann@users.noreply.github.com>
2025-08-17 20:31:18 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -6,29 +6,18 @@ import LandingHome from './pages/LandingHome';
 import CustomerPage from './customerpages/Customer';
 import Waitlist from './pages/Waitlist';
 
-
 function App() {
-  
-
-  
-
   return (
     <div className="min-h-screen bg-white">
       <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/waitlist" element={<Waitlist />} />
-        <Route path="/dasboard" element={<LandingHome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/customer" element={< CustomerPage />} />
-        {/* Add more routes like this: */}
-        {/* <Route path="/login" element={<Login />} /> */}
-      </Routes>
-    </Router>
-
-
-
-      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/waitlist" element={<Waitlist />} />
+          <Route path="/dashboard" element={<LandingHome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/customer" element={<CustomerPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
